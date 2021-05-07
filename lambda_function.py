0ebd22a7d3ec5c8af3a70eec0552aa50ef191de1
Heena8390 <83802251+Heena8390@users.noreply.github.com>
2021-05-07 11:09:42 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import json
 def lambda_handler(event, context):
     # TODO implement
     
-    print("these changes should be reflected in lambda after commit")
+    print("these changes should be reflected in lambda after commit to new branch")
     print("second push")
     #print("push from branch2 to master")
     return {"status":200, "body":json.dumps("hello from lambda and github,demo for push to master branch")}
